97fe6f06918f25f092d3d5c5c3aee02194160d5a
Israel <issupl@gmail.com>
2021-01-14 18:11:13 +0100

    Otra subida de archivo - deleteEntry.js

---
+++ BACK/controllers/entries/deleteEntry.js
@@ -0,0 +1,24 @@
+const getDB = require("../../db");
+
+const deleteEntry = async (req, res, next) => {
+
+    let conexion;
+    try{
+        conexion = await getDB();
+        const {id} = req.params;
+        console.log(`esto es lo que recibo ${id}`);
+
+        await conexion.query(`DELETE FROM usuarios WHERE id_usu = ?`,[id]);
+        res.send({
+            status: "ok",
+            message: `La entrada con id ${id} fué borrado`,
+          });
+    } catch(error){
+        next(error);
+    } finally{
+        if (conexion) conexion.release();
+    }
+    
+}
+
+module.exports = deleteEntry;

ba26e672f5cb5d1c1072220dd6a60c55fbe8b913
Israel <issupl@gmail.com>
2021-01-19 14:29:58 +0100

    archivo nuevo usuario y helpers

---
+++ BACK/controllers/entries/newUser.js
@@ -0,0 +1,76 @@
+const getDB = require("../../db");
+const {generateRandomString,sendMail} = require("../../helpers");
+
+const newEntry = async (req, res, next) => {
+    let conexion;
+
+    try{
+        conexion = await getDB();
+
+            // Recojo de req.body el email y la password
+
+    const { email, password } = req.body;
+
+    // Compruebo que no estén vacíos
+
+    if (!email || !password) {
+      const error = new Error("Faltan campos");
+      error.httpStatus = 400;
+      throw error;
+    }
+    
+      // Compruebo que no exista un usuario en la base de datos con ese email
+
+      const [existingUser] = await conexion.query(
+        `
+        SELECT id_usu
+        FROM usuarios
+        WHERE mail=?
+      `,
+        [email]
+      );
+
+      
+    if (existingUser.length > 0) {
+        const error = new Error(
+          "Ya hay un usuario en la base de datos con ese email"
+        );
+        error.httpStatus = 409;
+        throw error;
+      }
+  
+  
+        const { nomUsuario_usu, nom_usu, ape1_usu, ape2_usu, biografia_usu, dni_usu } = req.body; 
+          // Creo un código de registro (contraseña temporal de un solo uso)
+        const registrationCode = generateRandomString(10);
+        await conexion.query(
+            `
+            INSERT INTO usuarios (nomUsuario_usu,nom_usu,ape1_usu,ape2_usu,biografia_usu,dni_usu,mail, pwd, codigoderegistro)
+            VALUES (?,?,?,?,?,?,?,SHA2(?,512),?);
+        `,[nomUsuario_usu,nom_usu,ape1_usu,ape2_usu,biografia_usu,dni_usu, email, password, registrationCode]);
+
+
+            // ${process.env.PUBLIC_HOST}/users/validate/${registrationCode}
+        const emailBody = `Te acabas de registrar en Servicios Digitales.
+                            Pulsa en este link para verificar tu mail: ${process.env.PUBLIC_HOST}/validar/${registrationCode}`;
+
+        await sendMail({
+            to: email,
+            subject: 'Activa tu usuario de Servicios Digitales',
+            body: emailBody,
+
+        });
+
+
+        res.send({
+            menssage: "insertado?",
+        });
+
+    } catch(error){
+        next(error);
+    } finally {
+        if (conexion) conexion.release();
+    }
+}
+
+module.exports = newEntry;
--- BACK/helpers.js
+++ BACK/helpers.js
@@ -1,5 +1,37 @@
 const { format } = require("date-fns");
+const sgMail = require('@sendgrid/mail');
+const crypto = require("crypto");
+sgMail.setApiKey('');
+//se manda como un objeto, y este ya lo desestructura en 'to','subject' y 'body'
+async function sendMail({to,subject,body}){
+    try{
+            const msg = {
+                    to,
+                    from: process.env.SENDGRID_FROM, // Use the email address or domain you verified above
+                    subject,
+                    text: body,
+                    html: `
+                    <div>
+                        <h1>${subject}</h1>
+                        <p>${body}</p>
+                    <strong>and easy to do anywhere, even with Node.js</strong>
+                    </div>
+                    `
+                };
+            await sgMail.send(msg);
+    }catch(error){
+        console.log(error)
+        throw new Error("Error enviando mail");
+    }
+  
 
+      
+}
+
+// Genera una cadena de caracteres aleatoria
+function generateRandomString(length) {
+    return crypto.randomBytes(length).toString("hex");
+  }
 function formatDateToDB(dateObject) {
   return format(dateObject, "yyyy-MM-dd HH:mm:ss");
 }
